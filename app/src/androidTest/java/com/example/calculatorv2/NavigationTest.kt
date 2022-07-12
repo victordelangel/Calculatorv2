3a5074c6eb36fb78651e50589fe95cc4eda2218c
Victor Del Angel <videla@microsoft.com>
2022-07-12 06:56:35 -0500

    Actualización proyecto

--- app/src/androidTest/java/com/example/calculatorv2/NavigationTest.kt
+++ app/src/androidTest/java/com/example/calculatorv2/NavigationTest.kt
@@ -1,37 +1,25 @@
 package com.example.calculatorv2
 
 
-import androidx.test.espresso.DataInteraction
-import androidx.test.espresso.ViewInteraction
-import androidx.test.filters.LargeTest
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewParent
-
-import androidx.test.InstrumentationRegistry.getInstrumentation
-import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.pressBack
-import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
-import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
-
-import com.example.calculatorv2.R
-
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import org.hamcrest.Description
 import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
-import org.hamcrest.core.IsInstanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.Matchers.anything
-import org.hamcrest.Matchers.`is`
+import com.microsoft.appcenter.espresso.Factory
+import com.microsoft.appcenter.espresso.ReportHelper
+import org.junit.After
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -41,57 +29,90 @@ class NavigationTest {
     @JvmField
     var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
+    @Rule
+    @JvmField
+    var reportHelper = Factory.getReportHelper()
+
     @Test
     fun navigationTest() {
         val bottomNavigationItemView = onView(
-allOf(withId(R.id.navigation_home), withContentDescription("Home"),
-childAtPosition(
-childAtPosition(
-withId(R.id.nav_view),
-0),
-0),
-isDisplayed()))
+            allOf(
+                withId(R.id.navigation_home), withContentDescription("Home"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_view),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
         bottomNavigationItemView.perform(click())
-        
+
         val bottomNavigationItemView2 = onView(
-allOf(withId(R.id.navigation_dashboard), withContentDescription("Dashboard"),
-childAtPosition(
-childAtPosition(
-withId(R.id.nav_view),
-0),
-1),
-isDisplayed()))
+            allOf(
+                withId(R.id.navigation_dashboard), withContentDescription("Dashboard"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_view),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
         bottomNavigationItemView2.perform(click())
-        
+
         val bottomNavigationItemView3 = onView(
-allOf(withId(R.id.navigation_notifications), withContentDescription("Notifications"),
-childAtPosition(
-childAtPosition(
-withId(R.id.nav_view),
-0),
-2),
-isDisplayed()))
+            allOf(
+                withId(R.id.navigation_notifications), withContentDescription("Notifications"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_view),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
         bottomNavigationItemView3.perform(click())
-        
+
         val bottomNavigationItemView4 = onView(
-allOf(withId(R.id.navigation_home), withContentDescription("Home"),
-childAtPosition(
-childAtPosition(
-withId(R.id.nav_view),
-0),
-0),
-isDisplayed()))
+            allOf(
+                withId(R.id.navigation_home), withContentDescription("Home"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_view),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
         bottomNavigationItemView4.perform(click())
-        
+
         val textView = onView(
-allOf(withId(R.id.text_home), withText("This is home Fragment"),
-withParent(withParent(withId(R.id.nav_host_fragment_activity_main))),
-isDisplayed()))
+            allOf(
+                withId(R.id.text_home), withText("This is home Fragment"),
+                withParent(withParent(withId(R.id.nav_host_fragment_activity_main))),
+                isDisplayed()
+            )
+        )
         textView.check(matches(withText("This is home Fragment")))
-        }
-    
+    }
+
+    @After
+    fun tearDown(){
+        reportHelper.label("Finishing test")
+    }
+
     private fun childAtPosition(
-            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
 
         return object : TypeSafeMatcher<View>() {
             override fun describeTo(description: Description) {
@@ -106,4 +127,4 @@ isDisplayed()))
             }
         }
     }
-    }
+}
